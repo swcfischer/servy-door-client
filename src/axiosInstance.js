0879a9eefc0f4d8ec442f79a660fcd59fce7fbb7
Steven Fischer <swcfischer@gmail.com>
2025-02-11 15:48:51 -0600

    changes

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:8888";
+  process.env.NODE_ENV === "production"
+    ? "https://servy-door-server.onrender.com"
+    : "http://localhost:8888";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
