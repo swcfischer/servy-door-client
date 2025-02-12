4d9b66422606ff958fb7319f8db72167f61a1883
Steven Fischer <swcfischer@gmail.com>
2025-02-11 20:45:26 -0600

    changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -146,11 +146,7 @@ const Layout = ({ children }) => {
             <Typography variant="body2" className="typography">
               ServyDoor{" "}
             </Typography>
-            <a
-              target="_blank"
-              href="https://github.com/swcfischer"
-              id="attribution"
-            >
+            <a target="_blank" href="https://stevefischer.dev" id="attribution">
               Steve Fischer
             </a>
           </footer>
