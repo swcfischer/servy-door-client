ed24f1b22847a97350a9d2d16863c83d09a6f9d4
Steven Fischer <swcfischer@gmail.com>
2025-02-17 07:55:32 -0600

    changing bestseller title

--- src/components/BestSellerList.jsx
+++ src/components/BestSellerList.jsx
@@ -2,17 +2,42 @@ import React from "react";
 import styled from "@emotion/styled";
 import bestsellerList from "../constants/bestsellers";
 import BestsellerItem from "./BestsellerItem";
+import { BsDoorOpen } from "react-icons/bs";
 
 const Container = styled.div`
   transition: opacity 0.5s ease-in-out;
   opacity: ${(props) => (props.isLoading ? 0 : 1)};
+
+  .besterseller-list__title {
+    padding-top: 0;
+    margin-top: 0;
+    font-style: italic;
+    font-weight: normal;
+    font-size: 32px;
+    display: flex;
+    align-items: center;
+
+    .bestseller-list__icon {
+      position: relative;
+      top: 4px;
+      display: inline-block;
+      padding-left: 12px;
+      font-size: 28px;
+    }
+  }
 `;
 
 function BestSellerList(props) {
   const { isLoading } = props;
+
   return (
-    <Container isLoading={isLoading}>
-      <h2 style={{ paddingTop: "0", marginTop: "0" }}>Bestsellers</h2>
+    <Container className="bestseller-list" isLoading={isLoading}>
+      <h2 className="besterseller-list__title">
+        Reading Opens Doors!
+        <span className="bestseller-list__icon">
+          <BsDoorOpen />
+        </span>
+      </h2>
 
       <div className="book-list-container">
         {bestsellerList.map(({ id, volumeInfo }) => (
