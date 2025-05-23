0b8f3b75c780551754440330ad91439ec2426bac
Steven Fischer <swcfischer@gmail.com>
2025-05-23 03:16:53 -0500

    changes

---
+++ src/pages/blog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Blog = () => (
+  <iframe
+    src="https://avidreading.com/"
+    title="Avid Reading Blog"
+    style={{
+      width: "100%",
+      height: "100vh",
+      border: "none",
+    }}
+    allowFullScreen
+  />
+);
+
+export default Blog;
