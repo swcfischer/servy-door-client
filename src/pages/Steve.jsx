ca2cd97f4a6bf3a1fa42aee08634d9df5e14ae5e
Steven Fischer <swcfischer@gmail.com>
2025-07-14 22:39:11 +0100

    changes

--- src/pages/Steve.jsx
+++ src/pages/Steve.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const links = [
-  {
-    name: "Small UI Fixes for Reading Application",
-    url: "https://www.youtube.com/watch?v=4X25XEoErHA&ab_channel=StevenFischer",
-  },
+  // {
+  //   name: "Small UI Fixes for Reading Application",
+  //   url: "https://www.youtube.com/watch?v=4X25XEoErHA&ab_channel=StevenFischer",
+  // },
   {
     name: "GitHub Copilot Versus Jr. Dev. ",
     url: "https://www.youtube.com/watch?v=vjOFLjSa8dU&t=3s",
