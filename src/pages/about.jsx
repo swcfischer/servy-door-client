f0ee4b914bfb835744175d3567db2c83c64458df
Steven Fischer <swcfischer@gmail.com>
2025-10-27 22:15:13 -0500

    change

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  h1 {
+    text-align: center;
+  }
+
+  p {
+    font-size: 20px;
+    max-width: 370px;
+    margin: 0 auto;
+    line-height: 28px;
+  }
+`;
+
 function About(props) {
   return (
-    <div>
+    <Container>
       <h1>About ServyDoor</h1>
       <p>
-        Welcome to ServyDoor, your ultimate reading companion! ServyDoor is a
-        powerful application designed to help you bookmark your favorite
-        readings, stay on track with your progress, and keep detailed notes for
-        each reading session. Whether you're a casual reader or an avid book
-        lover, ServyDoor is here to enhance your reading experience.
+        Welcome to <strong>ServyDoor</strong>, your ultimate reading companion!{" "}
+        <strong>ServyDoor</strong> is a powerful application designed to help
+        you bookmark your favorite <strong>books</strong>, stay on track with
+        your progress, and keep detailed notes for each reading session.
       </p>
-    </div>
+      <br />
+
+      <p>
+        Whether you're a casual reader or an avid book-lover,{" "}
+        <strong>ServyDoor</strong> is here to enhance your reading experience.
+      </p>
+    </Container>
   );
 }
 
