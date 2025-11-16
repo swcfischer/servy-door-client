4041001b43a8175a5b2b8a41c96032c96ce7917f
Steven Fischer <swcfischer@gmail.com>
2025-11-16 10:37:05 -0600

    Update book container styles: change background color, add margin, and border

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -117,7 +117,9 @@ const Container = styled.div`
     scrollbar-width: thin; /* Firefox */
     scrollbar-color: #c0a427#1e1e1e; /* thumb track */
     padding: 12px 24px 24px 24px;
-    background: #d6af04;
+    background: #d4aa01;
+    margin: 0;
+    border: 1px solid #222;
 
     &::-webkit-scrollbar {
       width: 10px;
