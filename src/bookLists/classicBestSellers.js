b94b628a8826260983c05cfed765c601c384e1bc
Steven Fischer <swcfischer@gmail.com>
2025-11-19 05:03:46 -0600

    Add "The Oxygen Advantage" to classic bestseller list with detailed information

--- src/bookLists/classicBestSellers.js
+++ src/bookLists/classicBestSellers.js
@@ -1,6 +1,111 @@
 const classicBestsellerList = [
   {
     kind: "books#volume",
+    id: "l7_3BQAAQBAJ",
+    etag: "gTH5rfHQGIo",
+    selfLink: "https://www.googleapis.com/books/v1/volumes/l7_3BQAAQBAJ",
+    volumeInfo: {
+      title: "The Oxygen Advantage",
+      subtitle:
+        "The Simple, Scientifically Proven Breathing Techniques for a Healthier, Slimmer, Faster, and Fitter You",
+      authors: ["Patrick McKeown"],
+      publisher: "HarperCollins",
+      publishedDate: "2015-09-15",
+      description:
+        "<p>A simple yet revolutionary approach to improving your body’s oxygen use, increasing your health, weight loss, and sports performance—whether you’re a recovering couch potato or an Ironman triathlon champion. With a foreword by New York Times bestselling author Dr. Joseph Mercola.</p><p>Achieve more with less effort: The secret to weight loss, fitness, and wellness lies in the most basic and most overlooked function of your body—how you breathe. One of the biggest obstacles to better health and fitness is a rarely identified problem: chronic over-breathing. We often take many more breaths than we need—without realizing it—contributing to poor health and fitness, including a host of disorders, from anxiety and asthma to insomnia and heart problems.</p><p>In The Oxygen Advantage, the man who has trained over 5,000 people—including Olympic and professional athletes—in reduced breathing exercises now shares his scientifically validated techniques to help you breathe more efficiently. Patrick McKeown teaches you the fundamental relationship between oxygen and the body, then gets you started with a Body Oxygen Level Test (BOLT) to determine how efficiently your body uses oxygen. He then shows you how to increase your BOLT score by using light breathing exercises and learning how to simulate high altitude training, a technique used by Navy SEALs and professional athletes to help increase endurance, weight loss, and vital red blood cells to dramatically improve cardio-fitness.</p><p>Following his program, even the most out-of-shape person (including those with chronic respiratory conditions such as asthma) can climb stairs, run for a bus, or play soccer without gasping for air, and everyone can achieve:</p><ul> <li> Easy weight loss and weight maintenance</li> <li> Improved sleep and energy</li> <li> Increased concentration</li> <li> Reduced breathlessness during exercise</li> <li> Heightened athletic performance</li> <li> Improved cardiovascular health</li> <li> Elimination of asthmatic symptoms, and more.</li></ul><p>With The Oxygen Advantage, you can look better, feel better, and do more—it’s as easy as breathing.</p>",
+      industryIdentifiers: [
+        { type: "ISBN_10", identifier: "0062349481" },
+        { type: "ISBN_13", identifier: "9780062349484" },
+      ],
+      readingModes: { text: true, image: false },
+      pageCount: 288,
+      printedPageCount: 315,
+      printType: "BOOK",
+      categories: [
+        "Health & Fitness / Diet & Nutrition / Weight Loss",
+        "Health & Fitness / Diet & Nutrition / Nutrition",
+        "Health & Fitness / Healthy Living & Personal Hygiene",
+        "Health & Fitness / Exercise / Strength Training",
+        "Health & Fitness / Exercise / General",
+        "Health & Fitness / Diet & Nutrition / Diets",
+        "Medical / Mental Health",
+        "Medical / Alternative & Complementary Medicine",
+        "Body, Mind & Spirit / Healing / General",
+        "Science / Life Sciences / Biology",
+      ],
+      maturityRating: "NOT_MATURE",
+      allowAnonLogging: true,
+      contentVersion: "1.5.5.0.preview.2",
+      panelizationSummary: {
+        containsEpubBubbles: false,
+        containsImageBubbles: false,
+      },
+      imageLinks: {
+        smallThumbnail:
+          "http://books.google.com/books/publisher/content?id=l7_3BQAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE72NuJc_AG1FJCWect6Kf5mPVCmjEKa4YJcjNwewfGBWwrYfhAWbP88glxYbv4aYDo42SAtKLpgE8iZptec0rsKtIjXYxrrqe7G9Mayy6LpCqB6dNeyUASk2mgpuUROgTJLi0cvA&source=gbs_api",
+        thumbnail:
+          "http://books.google.com/books/publisher/content?id=l7_3BQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE73l_0VJgdMwUEGDpmUY0uFxq_q9WspupACGGa6WgkHelB2tOTuo1B_LcWQ4jfFosovVWJUnWavZl6NececxY1f3xf8gBhzECO954YoesrPSEoibG8k4laKDeHK8Q4Zlf7wR8gN4&source=gbs_api",
+        small:
+          "http://books.google.com/books/publisher/content?id=l7_3BQAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&imgtk=AFLRE73d1O1rdnnEEOlklcXTGGHR47WVP-41Groej-1jbuCWmvghSA8iuU08FHBT5vB35rtOClF9vofzyZC2hH2jMUM89MgV4H1KCRYg344ZICeyJH963ypnyxpCeGqLKTSbVcWixfQs&source=gbs_api",
+        medium:
+          "http://books.google.com/books/publisher/content?id=l7_3BQAAQBAJ&printsec=frontcover&img=1&zoom=3&edge=curl&imgtk=AFLRE733s3pN2BuYdiTDIxHuop-jhZjkyOYJ4wADZLpaJ2ytmAnlYq_e_1EA4p68lC2BP4rjqi92W0EyD9GjZuIZAvb1uvwJQcZ7fMKoVuqfsOTS92hJR8m85dKtuH_NyRU_IiZzPCJA&source=gbs_api",
+        large:
+          "http://books.google.com/books/publisher/content?id=l7_3BQAAQBAJ&printsec=frontcover&img=1&zoom=4&edge=curl&imgtk=AFLRE71KYG3sydUtynVyGrqBg21OnIMGl_aHj3mNzxyYJEN4OfWJaEjie1wamrz09IBKgcmynhP8tI6kY2Y_ZY2U8rQtJRMqLK8B1mKNx8GaAF4SdrSPJxKX3j5r3b7_3713P03CYwVc&source=gbs_api",
+        extraLarge:
+          "http://books.google.com/books/publisher/content?id=l7_3BQAAQBAJ&printsec=frontcover&img=1&zoom=6&edge=curl&imgtk=AFLRE71dg_4v7fCIe3s4SbYah5UJNY_cesfCC5SuRjn5_C_u7lxUcV37uYnVUxJXBraHiOGYXFP5iigQXezwfhcrQ17SIKiRJED3t_hbbETyEm4V5APjOEVQBjiG3jsLKv1gNXXhiKKJ&source=gbs_api",
+      },
+      language: "en",
+      previewLink:
+        "http://books.google.com/books?id=l7_3BQAAQBAJ&hl=&source=gbs_api",
+      infoLink:
+        "https://play.google.com/store/books/details?id=l7_3BQAAQBAJ&source=gbs_api",
+      canonicalVolumeLink:
+        "https://play.google.com/store/books/details?id=l7_3BQAAQBAJ",
+    },
+    layerInfo: { layers: [{ layerId: "geo", volumeAnnotationsVersion: "7" }] },
+    saleInfo: {
+      country: "US",
+      saleability: "FOR_SALE",
+      isEbook: true,
+      listPrice: { amount: 8.49, currencyCode: "USD" },
+      retailPrice: { amount: 8.49, currencyCode: "USD" },
+      buyLink:
+        "https://play.google.com/store/books/details?id=l7_3BQAAQBAJ&rdid=book-l7_3BQAAQBAJ&rdot=1&source=gbs_api",
+      offers: [
+        {
+          finskyOfferType: 1,
+          listPrice: { amountInMicros: 8490000, currencyCode: "USD" },
+          retailPrice: { amountInMicros: 8490000, currencyCode: "USD" },
+          giftable: true,
+        },
+      ],
+    },
+    accessInfo: {
+      country: "US",
+      viewability: "PARTIAL",
+      embeddable: true,
+      publicDomain: false,
+      textToSpeechPermission: "ALLOWED",
+      epub: {
+        isAvailable: true,
+        acsTokenLink:
+          "http://books.google.com/books/download/The_Oxygen_Advantage-sample-epub.acsm?id=l7_3BQAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api",
+      },
+      pdf: { isAvailable: false },
+      webReaderLink:
+        "http://play.google.com/books/reader?id=l7_3BQAAQBAJ&hl=&source=gbs_api",
+      accessViewStatus: "SAMPLE",
+      quoteSharingAllowed: false,
+    },
+    img: {
+      width: 1744,
+      height: 2632,
+      image:
+        "http://books.google.com/books/publisher/content?id=l7_3BQAAQBAJ&printsec=frontcover&img=1&zoom=6&edge=curl&imgtk=AFLRE71dg_4v7fCIe3s4SbYah5UJNY_cesfCC5SuRjn5_C_u7lxUcV37uYnVUxJXBraHiOGYXFP5iigQXezwfhcrQ17SIKiRJED3t_hbbETyEm4V5APjOEVQBjiG3jsLKv1gNXXhiKKJ&source=gbs_api",
+    },
+  },
+  {
+    kind: "books#volume",
     id: "RjBOEQAAQBAJ",
     etag: "Ir1V8YIigwI",
     selfLink: "https://www.googleapis.com/books/v1/volumes/RjBOEQAAQBAJ",
