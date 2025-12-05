b212fe6943e05f65e49fe2c7567d06b06759e009
Steven Fischer <swcfischer@gmail.com>
2025-12-05 13:24:13 -0600

    Update app name in gatsby-config and replace logo image

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `servydoor-app`,
         short_name: `ServyDoor`,
         start_url: `/`,
         background_color: `#222`,
