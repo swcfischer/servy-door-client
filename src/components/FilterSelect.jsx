a100cc45c6297e799e9c33e920138b004857153a
Steven Fischer <swcfischer@gmail.com>
2025-12-13 08:28:27 -0600

    Update AvidTrending component to improve accessibility and clarity of YouTube link

--- src/components/FilterSelect.jsx
+++ src/components/FilterSelect.jsx
@@ -192,12 +192,12 @@ function AvidTrending(props) {
 
   if (!wasSearchDone(books, q, params)) {
     return (
-      <div id="Accessible YouTube">
+      <div id="Accessible YouTube" style={{ marginLeft: "12px" }}>
         <a
           href="https://www.avidlanguagelearning.com/app/video/5ffed861-e3f1-4c3e-8d62-3d39ffebc4f2"
           target="_blank"
         >
-          YT Lanuage Learning
+          YouTube Viewer
         </a>
       </div>
     );
